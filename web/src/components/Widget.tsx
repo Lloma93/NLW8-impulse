b02df5cafcec56a8d646570fe7f8202ebd81bf40
Palloma <palloma.adolfi@gmail.com>
2022-06-22 14:28:07 -0300

    erro vercel import

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ChatTeardropDots } from "phosphor-react";
+// import { ChatTeardropDots } from "phosphor-react";
 import { Popover } from '@headlessui/react'
 import { WidgetForm } from './WidgetForm';
 
@@ -11,7 +11,8 @@ export function Widget() {
             
             <Popover.Panel><WidgetForm /></Popover.Panel>
         <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
-            <ChatTeardropDots className="w-6 h-6"/>
+            {/* <ChatTeardropDots className="w-6 h-6"/> */}
+            erro import
             <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear"> 
             <span className="pl-2">
             </span>
